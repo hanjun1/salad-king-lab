046b5ee6a8b440f1fa3abaa76c2b4660b90c8c8f
Ryan Kim <hanjun1@ualberta.ca>
2021-04-14 10:30:38 -0600

    added salad king lab solutions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    currentSpice: "Medium",
+    status: "A little kick",
+    levels: [
+      "A little kick",
+      "Nice",
+      "Start mopping your brow",
+      "Watch out",
+      "Are you sure?",
+      "Some like it hot",
+      "May cause stomach upset",
+    ],
+  };
+  updateState = () => {
+    if (this.state.currentSpice >= 20) {
+      this.setState({ status: this.state.levels[6] });
+    } else if (this.state.currentSpice >= 15) {
+      this.setState({ status: this.state.levels[5] });
+    } else if (this.state.currentSpice >= 10) {
+      this.setState({ status: this.state.levels[4] });
+    } else if (this.state.currentSpice >= 5) {
+      this.setState({ status: this.state.levels[3] });
+    } else if (this.state.currentSpice >= 3) {
+      this.setState({ status: this.state.levels[2] });
+    } else if (this.state.currentSpice >= 1) {
+      this.setState({ status: this.state.levels[1] });
+    } else {
+      this.setState({ status: this.state.levels[0] });
+    }
+  };
+  addSpiceLevel = () => {
+    if (this.state.currentSpice === "Medium") {
+      this.setState({ currentSpice: 1 }, () => {
+        this.updateState();
+      });
+    } else {
+      this.setState(
+        (state) => ({ currentSpice: state.currentSpice + 1 }),
+        () => {
+          this.updateState();
+        }
+      );
+    }
+  };
+  removeSpiceLevel = () => {
+    if (this.state.currentSpice === 1 || this.state.currentSpice === "Medium") {
+      this.setState({ currentSpice: "Medium" }, () => {
+        this.updateState();
+      });
+    } else {
+      this.setState(
+        (state) => ({ currentSpice: state.currentSpice - 1 }),
+        () => {
+          this.updateState();
+        }
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Salad King Pepper Chart SPA</h1>
+        <div>
+          <button onClick={() => this.addSpiceLevel()}>Add Some Heat</button>
+          <button onClick={() => this.removeSpiceLevel()}>Cool It Off</button>
+        </div>
+        <h2>Spice Level: {this.state.currentSpice}</h2>
+        <h2>Status: {this.state.status}</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
